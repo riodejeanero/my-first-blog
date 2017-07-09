655be268afee6c27f1720d23bfc1914c17d27eae
rioabella <rioabella@gmail.com>
2017-07-09 20:41:53 +0000

    edit settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*!cxuee80l$*r1anen1m@eyl@2$*7d!bw040!h^w%iqu2ezlfi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<rabella>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'rabella.pythonanywhere.com']
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'CA'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
